d1687b9af3ec953994ccb32ed2ae56fe14e299c9
Ravi Simon <simonravi@yahoo.com>
2018-10-22 00:07:29 +0530

    Completed functionality of hello_world and greet function

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,15 @@
+# Python3
 
 
 def hello_world():
-    return ""
+    return "Hello World"
 
 
 def greet(name):
-    return ""
+    return "Hello %s" % name
+
+
+if __name__ == '__main__':
+    print(hello_world())
+    name = input("Enter your name: ")
+    print(greet(name))
